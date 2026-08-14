9d87f4592084a723d57b439860e220b754944084
Koko <koko@Pichs-MacBook-Pro.local>
2026-08-14 08:48:48 +0700

    Improve PWA install and assemble flags

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "flag-draw-v5";
+const CACHE = "flag-draw-v6";
 const CORE_ASSETS = ["/", "/index.html", "/manifest.json", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", (event) => {
